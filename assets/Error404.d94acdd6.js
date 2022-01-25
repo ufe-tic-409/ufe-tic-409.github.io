e94c646712d69f848eb1803a76931e01879d7366
kagawish <karimgawish.1@gmail.com>
2022-01-25 07:06:47 +0200

    [Add] Adds build

--- assets/Error404.d94acdd6.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./index.d71536dc.js";import{e}from"./vendor.e65a07f6.js";const o=e({name:"Error404"});function n(t,s,a,c,d,f){return" 404 "}var i=r(o,[["render",n]]);export{i as default};
