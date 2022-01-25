19813a54eebc2fd0d8e330f24f6aa6a70bc3ab33
kagawish <karimgawish.1@gmail.com>
2022-01-25 08:30:20 +0200

    Build

---
+++ dist/assets/Error404.1dc0204e.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./index.7f51fe11.js";import{e}from"./vendor.e65a07f6.js";const o=e({name:"Error404"});function n(t,s,a,f,p,_){return" 404 "}var i=r(o,[["render",n]]);export{i as default};
--- dist/assets/Error404.73aceb00.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./index.bf4c2d41.js";import{e}from"./vendor.e65a07f6.js";const o=e({name:"Error404"});function n(t,s,a,f,c,p){return" 404 "}var i=r(o,[["render",n]]);export{i as default};
