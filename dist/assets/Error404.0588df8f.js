395604a52843119d759f6d3f784742541e29222b
kagawish <karimgawish.1@gmail.com>
2022-01-25 07:06:47 +0200

    [Add] Adds build

---
+++ dist/assets/Error404.0588df8f.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./index.fc673c5f.js";import{e}from"./vendor.e65a07f6.js";const o=e({name:"Error404"});function n(t,s,a,f,c,p){return" 404 "}var i=r(o,[["render",n]]);export{i as default};
